20c1bd658ed2da86cf717d5709445269c5110bd0
GedSargent <120935232+GedSargent@users.noreply.github.com>
2023-01-18 22:51:45 +0000

    Add app instructions

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ListItem from "./components/ListItem";
+import Paragraph from "./components/Paragraph";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex items-center justify-center min-h-screen max-w-2xl mx-auto">
+      <div className="p-20 rounded-xl bg-sky-100">
+        <h1 className="text-6xl font-bold text-blue-500 mb-8 text-center">
+          Hello world!
+        </h1>
+        <Paragraph>
+          Your task is to replace this content with code that will build a ToDo
+          app. It should allow the user to:
+        </Paragraph>
+        <ol className="my-4 ml-16">
+          <ListItem>Create a ToDo</ListItem>
+          <ListItem>Delete ToDos</ListItem>
+          <ListItem>Edit ToDos</ListItem>
+        </ol>
+        <Paragraph>
+          You should also ensure the app is styled professionally using Tailwind
+          CSS.
+        </Paragraph>
+        <Paragraph>
+          Be sure to use an online Tailwind CSS cheatsheet like{" "}
+          <a
+            className="text-blue-500 hover:text-blue-700 underline"
+            href="https://nerdcave.com/tailwind-cheat-sheet"
+          >
+            this one
+          </a>{" "}
+          online to help you.
+        </Paragraph>
+      </div>
     </div>
   );
 }
