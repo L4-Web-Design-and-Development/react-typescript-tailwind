7029a59f2570ea0af15c95a5671b067f69e1cf8c
GedSargent <120935232+GedSargent@users.noreply.github.com>
2023-01-19 09:29:17 +0000

    Add counter challenge

--- src/App.tsx
+++ src/App.tsx
@@ -7,36 +7,45 @@ function App() {
     <div className="flex items-center justify-center min-h-screen container mx-auto">
       <div className="m-8 p-20 rounded-xl bg-sky-100 shadow-xl">
         <h1 className="text-6xl font-bold text-blue-500 mb-8 text-center">
-          Hello world!
+          Counter
         </h1>
         <Paragraph>
-          This project has multiple branches to help you get used to coding in
-          React.
+          Can you update the component code so that clicking the 'Add' button
+          below makes the number shown increment by one every time?
         </Paragraph>
-        <Paragraph>
-          Use GitHub Desktop to switch between the branches and complete the
-          tasks. Create your own branches from these to work on your solutions
-          independently from the original code.
-        </Paragraph>
-        <div className="m-8 p-10 rounded-xl bg-white">
+        <div className="p-10 pb-5 bg-white rounded-lg mb-6">
           <Paragraph>
-            A list of the branches and their recommended order is shown below.
+            <strong>HINT</strong>: you can use the{" "}
+            <Link url="https://beta.reactjs.org/reference/react/useState#reference">
+              useState
+            </Link>{" "}
+            hook to solve this. Look at a usage example{" "}
+            <Link url="https://beta.reactjs.org/reference/react/useState#usage">
+              here
+            </Link>
+            .
           </Paragraph>
-          <ol className="my-4 ml-16">
-            <ListItem>Counter</ListItem>
-            <ListItem>Handling User Input</ListItem>
-            <ListItem>ToDo List App</ListItem>
-          </ol>
+          <Paragraph>
+            If you are REALLY stuck, take a look at a counter example from the
+            React docs page{" "}
+            <Link url="https://beta.reactjs.org/reference/react/useState#examples-basic">
+              here.
+            </Link>{" "}
+            .
+          </Paragraph>
         </div>
-        <Paragraph>
-          You should also ensure your solutions are styled professionally using{" "}
-          <Link url="https://tailwindcss.com/">Tailwind CSS</Link>.
-        </Paragraph>
-        <Paragraph>
-          Be sure to use an online Tailwind CSS cheatsheet like{" "}
-          <Link url="https://nerdcave.com/tailwind-cheat-sheet">this one</Link>{" "}
-          online to help you.
-        </Paragraph>
+        <div className="flex items-center justify-center p-10">
+          <div>
+            <div className="bg-white rounded-full flex items-center justify-center w-64 h-64">
+              <h2 className="text-indigo-700 text-6xl">1</h2>
+            </div>
+            <div className="mt-10 flex justify-center">
+              <button className="bg-indigo-700 text-white text-2xl px-12 py-6 rounded-lg">
+                Add
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
